718a4dde563d3e3c22aaa7090cf1f9e6682c5ba6
Marwari <bharattitop@gmail.com>
2018-03-19 20:16:38 +0530

    about page added.

--- FDS/urls.py
+++ FDS/urls.py
@@ -19,5 +19,6 @@ from dashboard import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index),
+    path('about/', views.about),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # view for index page
 def index(request):
     return render(request, 'index.html')
+# view for about page
+def about(request):
+    return  render(request, 'about.html')
